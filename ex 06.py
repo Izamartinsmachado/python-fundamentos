9e0d43e7f1633660f433f73c1867b18450bc4320
Izamartinsmachado <izadoramartinziza@gmail.com>
2026-01-06 21:58:34 -0300

    Add files via upload

---
+++ ex 06.py
@@ -0,0 +1,6 @@
+n= int(input('Digite um valor:'))
+print('O dobro de {} vale {}.'.format(n, n*2))
+print( 'O tripo de {} vale {}.'.format(n, n*3))
+print('A raiz quadrada de {} vale {:.3f}.'.format(n, n** (1/2)))
+
+
