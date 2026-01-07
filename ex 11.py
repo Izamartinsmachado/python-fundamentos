acf5ed63947e80bdd0666b967b21c3bf3e42e0f4
Izamartinsmachado <izadoramartinziza@gmail.com>
2026-01-06 22:06:09 -0300

    Add files via upload

---
+++ ex 11.py
@@ -0,0 +1,6 @@
+l = float(input('Qual a largura da parede?'))
+a = float(input('Qual a altura da parede?'))
+area = l*a
+print('A parede tem um total de {}m.'.format(area))
+tinta = area/2
+print('Para pintar a parede será necessário {}l de tinta.'.format(tinta))
