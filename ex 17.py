6060f8bccb38decfa440e40170c31e26bb39dd0a
Izamartinsmachado <izadoramartinziza@gmail.com>
2026-01-06 22:36:57 -0300

    Add files via upload

---
+++ ex 17.py
@@ -0,0 +1,7 @@
+import math
+co = float(input('Digite o valor do cateto oposto: '))
+ca = float(input('Digite o valor do cateto adjacente: '))
+hi = math.hypot(co, ca)
+print('A hipotenusa vai ser {:.2f}'.format(hi))
+
+
